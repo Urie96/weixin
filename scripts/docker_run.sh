a00a763a0251bf057dc671d2f092198cee98270c
Yang Rui <urie@mail.ustc.edu.cn>
2020-05-26 16:52:45 +0800

    fix

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,5 +1,6 @@
-docker-compose down
-docker rmi -f wxservice:latest
-docker build -t wxservice:latest .
+# docker-compose down
+# docker rmi -f wxservice:latest
+# docker build -t wxservice:latest .
+# docker rmi $(docker images | grep "none" | awk '{print $3}')
+docker-compose up --build -d
 docker rmi $(docker images | grep "none" | awk '{print $3}')
-docker-compose up -d
