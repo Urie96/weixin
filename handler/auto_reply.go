137fae3d734cb386199b88b51633dd2699767b98
Yang Rui <urie@mail.ustc.edu.cn>
2020-05-28 13:46:53 +0800

    test

--- handler/auto_reply.go
+++ handler/auto_reply.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/Urie96/weixin/util"
+
 	"github.com/Urie96/weixin/model"
 	"github.com/gin-gonic/gin"
 )
@@ -19,6 +21,7 @@ func autoReply(c *gin.Context) {
 	log.Println("msg body:\r\n" + string(b))
 	getmsg := &model.Msg{}
 	c.BindXML(getmsg)
+	util.PrintStruct(getmsg)
 	msg := model.NewMsg(getmsg.FromUserName)
 	msg.Content = "test"
 	c.Data(200, "application/xml", msg.ToXMLBytes())
