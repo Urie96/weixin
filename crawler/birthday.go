f00081ea6789e950c58ecde1e7b929b13f38e815
Yang Rui <urie@mail.ustc.edu.cn>
2020-05-29 17:05:32 +0800

    fix

---
+++ crawler/birthday.go
@@ -0,0 +1,29 @@
+package crawler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func GetSolar(year, month, day int) string {
+	reqBody := fmt.Sprintf("type=lunar&year=%d&month=%d&day=%d", year, month, day)
+	resp, err := http.Post("https://www.iamwawa.cn/home/nongli/ajax",
+		"application/x-www-form-urlencoded", strings.NewReader(reqBody))
+	checkError(err)
+	b, _ := ioutil.ReadAll(resp.Body)
+	model := &struct {
+		Data struct {
+			Solar string `json:"solar"`
+		} `json:"data"`
+	}{}
+	err = json.Unmarshal(b, model)
+	checkError(err)
+	solar := model.Data.Solar
+	solar = strings.ReplaceAll(solar, "年", "/")
+	solar = strings.ReplaceAll(solar, "月", "/")
+	solar = strings.ReplaceAll(solar, "日", "/")
+	return solar
+}
--- main.go
+++ main.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
-	"github.com/Urie96/weixin/util"
+	"github.com/Urie96/weixin/crawler"
 )
 
-type a struct {
-	Info string
-}
-
 func main() {
-	resp, err := http.Get("http://cmd.sweetlove.top?cmd=ls%20-l")
-	util.CheckError(err)
-	output, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(output))
+	for y := 2020; y < 2100; y++ {
+		solar := crawler.GetSolar(y, 6, 27)
+		fmt.Println(`"%s":true,`, solar)
+	}
+
 }
