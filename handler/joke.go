f2362b0a0f8b5a098710066e407a639a5896468d
Yang Rui <urie@mail.ustc.edu.cn>
2020-05-29 08:31:11 +0800

    Revert "fix"
    This reverts commit e684e57e88ebc525ad6a81f0998619adab2b0c58.

--- handler/joke.go
+++ handler/joke.go
@@ -1,14 +1,15 @@
 package handler
 
 import (
+	"github.com/Urie96/weixin/crawler"
 	"github.com/Urie96/weixin/dao"
 	"github.com/gin-gonic/gin"
 )
 
-// func createJokes(c *gin.Context) {
-// 	crawler.CrawlJokes()
-// 	c.String(200, "finished")
-// }
+func createJokes(c *gin.Context) {
+	crawler.CrawlJokes()
+	c.String(200, "finished")
+}
 
 func getAJoke(c *gin.Context) {
 	joke, err := dao.GetRandomJokeContent()
